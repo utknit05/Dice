da20b4fb49115bd8bb06d04c56f89dd8a4e85d60
Utkarsh <mac@Macs-MacBook-Pro.local>
2018-11-21 16:08:06 +0530

    Adding Dice rolling game

---
+++ Assets/Dice.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Dice : MonoBehaviour {
+    public Text score;
+    public Text highScore;
+
+    public void Start() {
+        highScore.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
+    }
+
+
+    public void RollDice() {
+        int number = Random.Range(1, 7);
+        score.text = number.ToString();
+
+        if (number > PlayerPrefs.GetInt("HighScore", 0))
+        {
+            PlayerPrefs.SetInt("HighScore", number);
+            highScore.text = number.ToString();
+        }
+    }
+
+    public void Reset()
+    {
+        PlayerPrefs.DeleteAll();
+        highScore.text = "0";
+        score.text = "0";
+    }
+}
